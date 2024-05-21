19c1c401e26362687dc52d564d7ca870ba804d05
Manik Singh <72637443+manik1080@users.noreply.github.com>
2024-05-21 21:24:53 +0530

    Update paper_scraper.py

--- paper_scraper.py
+++ paper_scraper.py
@@ -5,65 +5,34 @@ from serpapi import search as GoogleSearch
 
 class Scraper:
     def __init__(self):
-        '''
-                Defining headers to simulate browser visit to avoid scraper-block.
-                Headers rotate.
-        '''
-        self.user_agents = [
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
-        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
-        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-        ]
-        self.header_counter = 0
-        self.header_len = len(self.user_agents)
+        self.user_agents = []
+        self.num_agents = self.make_headers()
 
     def make_headers(self):
-        '''
-                Create Headers parameter and increment header_counter
-        '''
-        headers = {
-                  'User-Agent': self.user_agents[self.header_counter]
-                  }
-        self.header_counter = (self.header_counter + 1) % self.header_len
-        return headers
-    
-    def get_content(self, url: str):
-        '''
-                Get BeautifulSoup tree output
-        '''
+        url = "https://deviceatlas.com/blog/list-of-user-agent-strings"
+
         response = requests.get(url)
-        
+        webpage_content = response.content
+        soup = BeautifulSoup(webpage_content, 'html.parser')
+
+        self.user_agents = [{'User-Agent': i} for i in '\n'.join([j.text.strip() for j in soup.findAll('td')]).split('\n') if len(i)>0]
+        return len(self.user_agents)
+
+    def get_header(self):
+        return random.choice(self.user_agents)
+
+    def get_content(self, url: str):
+        head = self.get_header()
+        response = requests.get(url, headers=head)
         webpage_content = response.content
         soup = BeautifulSoup(webpage_content, 'html.parser')
         return soup
 
     def get_complete_content(self, url):
-        '''
-                Get text content of entire webpage.
-                    (Includes links, button texts and other noisy text)
-        '''
         def tag_visible(element):
             if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
                 return False
-            if isinstance(element, Comment):
+            if isinstance(element, bs4.element.Comment):
                 return False
             return True
 
@@ -80,15 +49,14 @@ class Scraper:
             return get_text(html)
         return text_from_html(url)
 
-    def extract(self, url: str, content=['title', 'abstract', 'references', 'links'], backup_title='NOT FOUND') -> dict:
-        '''
-                Extracting webpage content based on the content argument
-                Returns a python dictionary
-        '''
+    def extract(self, url: str, content=['title', 'abstract', 'references', 'links'], backup_title='NOT_FOUND') -> dict:
         similar = {'results': ['results', 'result', 'conclusion', 'conclusions'],
                    'references': ['references', 'citation', 'citations', 'reference', 'similar articles'],
                    }
+        #try:
         soup = self.get_content(url)
+        #except:
+         #   print(f"Exception occurred. Ignoring url {url}")
         head_list = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
         headings = soup.find_all(head_list)
         data = {}
@@ -109,30 +77,37 @@ class Scraper:
                         for sibling in heading.find_next_siblings():
                             for i in sibling.findChildren(href=True):
                                 if 'pubmed' in url:
+                                    if i.get('href').startswith('http'):
+                                        continue
                                     links[i.text.strip()] = "https://pubmed.ncbi.nlm.nih.gov" + i.get('href')
                                 else:
                                     links[i.text.strip()] = i.get('href')
                 data['links'] = links
 
             elif key=='abstract':
-                abstract = 'NOT FOUND'
+                abstract = 'NOT_FOUND'
                 for heading in headings:
                     head = heading.text.lower().strip()
                     if head==key:
                         abstract = heading.findNext('p').text.strip()
                         break
+                if len(abstract)<30:
+                    abstract = 'NOT_FOUND'
                 data['abstract'] = abstract
 
             elif key=='results':
-                results = 'NOT FOUND'
+                results = 'NOT_FOUND'
                 for heading in headings:
                     head = heading.text.lower().strip()
                     if head in similar[key]:
                         results = heading.findNext('p').text.strip()
                         break
+                if len(results)<30:
+                    abstract = 'NOT_FOUND'
                 data['results'] = results
+
             else:
-                other = 'NOT FOUND'
+                other = 'NOT_FOUND'
                 for heading in headings:
                     head = heading.text.lower().strip()
                     if head==key:
