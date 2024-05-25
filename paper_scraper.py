4aa1fbfdef1ba5accf6d6da1b44bab8efd32d0d9
Manik Singh <72637443+manik1080@users.noreply.github.com>
2024-05-25 21:13:45 +0530

    Adding ThreadedExtractor
    Now supports multi-threading

--- paper_scraper.py
+++ paper_scraper.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from serpapi import search as GoogleSearch
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import random
 import urllib
+import copy
 import bs4
 import re
+import pandas as pd
+import os
 
 
 class Scraper:
@@ -53,7 +57,7 @@ class Scraper:
             return get_text(html)
         return text_from_html(url)
 
-    def extract(self, url: str, content=['title', 'abstract', 'references', 'links'], backup_title='NOT_FOUND') -> dict:
+    def extract(self, url: str, content=['title', 'abstract', 'results', 'links'], backup_title='NOT_FOUND') -> dict:
         similar = {'results': ['results', 'result', 'conclusion', 'conclusions'],
                    'references': ['references', 'citation', 'citations', 'reference', 'similar articles'],
                    }
@@ -85,7 +89,8 @@ class Scraper:
                                         continue
                                     links[i.text.strip()] = "https://pubmed.ncbi.nlm.nih.gov" + i.get('href')
                                 else:
-                                    links[i.text.strip()] = i.get('href')
+                                    if i.get('href').startswith('http'):
+                                        links[i.text.strip()] = i.get('href')
                 data['links'] = links
 
             elif key=='abstract':
@@ -120,6 +125,124 @@ class Scraper:
                 data[key.lower().strip()] = other
         return data
 
+###########################################################
+######## Multi-scraping citations to csv/dataframe ########
+###########################################################
+    
+class ThreadedExtractor:
+    def __init__(self, max_workers, initial_url, content=['title', 'abstract', 'links']):
+        """Create ThreadedExtractor Object.
+
+        :param max_workers: number of concurrent threads
+        :type max_workers: int
+        :param initial_url: the first url to be scraped
+        :type initial_url: str
+        :param content: content to be scraped from url
+        :type content: list
+        :returns: ThreadedExtractor Object
+        :rtype: ThreadedExtractor
+        """
+        self.max_workers = max_workers
+        self.initial_url = initial_url
+        self.display_content = content
+        self.content = content if 'links' in content else content+['links']
+        self.scraper = Scraper()
+        self.scrape_single_url = lambda url: self.scraper.extract(url, self.content)
+        self.data = self.scrape_single_url(initial_url)
+        if not self.data['links']:
+            raise ValueError("No results. Try different initial url.")
+        self.df = {i: [self.data[i]] for i in self.display_content}
+
+    def extract(self, count, return_type='pandas', file_path=os.getcwd()):
+        """Extract files from citations.
+
+        :param count: number of urls to be scraped
+        :type count: int
+        :param return_type: function returns 'dict', 'pandas' or 'csv'
+        :type return_type: str
+        :param file_path: (if return_type is 'csv')Absolute path 
+        :type file_path: str
+        :returns: return_type specified
+        """
+        links = [i for i in list(self.data['links'].values())]
+        counter = 1
+        scraped_urls = set()
+        scraped_urls.add(self.initial_url)
+
+        while links and counter < count:
+            with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+                futures = {executor.submit(self.scrape_single_url, link):\
+                           link for link in links[:self.max_workers]}
+
+                results = []
+                for future in as_completed(futures):
+                    link = futures[future]
+                    try:
+                        data = future.result()
+                        results.append((link, data))
+                    except Exception as e:
+                        print(f"Error scraping {link}: {e}")
+
+            # Process all results after all threads have completed
+            for link, data in results:
+                if link not in scraped_urls:
+                    temp_df = copy.deepcopy(self.df)
+                    flag = False
+                    for column in self.display_content:
+                        if data[column]=='NOT_FOUND':
+                            flag = True
+                            break
+                        else:
+                            temp_df[column].append(data[column])
+                    
+                    if flag==True:
+                        scraped_urls.add(link)
+                    if flag==False:
+                        counter += 1
+                        self.df = copy.deepcopy(temp_df)
+                        scraped_urls.add(link)
+                        new_links = [i for i in list(data['links'].values()) if i not in scraped_urls]
+                        links.extend(new_links)
+
+            links = links[self.max_workers:]
+        if return_type=='dict':
+            return self.df
+        elif return_type=='pandas':
+            return pd.DataFrame(self.df)
+        elif return_type=='csv':
+            df = pd.DataFrame(self.df)
+            if file_path.endswith('csv'):
+                directory, name = os.path.split(file_path)
+            else:
+                directory, name = file_path, f"{self.initial_url.rstrip('/').split('/')[-1]}_{count}.csv"
+            output_path = os.path.join(directory, name)
+            df.to_csv(output_path, index=False)
+            print("Extracted ", counter, " files and saved at ", output_path)
+            return None
+        else:
+            raise ValueError("Incorrect return type\nMust be 'dict', 'pandas' or 'csv'")
+
+    def extend_pd(self, data, initial_url, count):
+        """Extend pandas dataframe.
+
+        :param data: pandas DataFrame object
+        :type data: pandas.DataFrame
+        :param initial_url: url to start scraping from
+        :type initial_url: str
+        :param count: number of rows to be added
+        :type count: int
+        :returns: pandas.DataFrame object
+        """
+        self.display_content = list(data.keys())
+        self.data = self.scrape(initial_url, self.content)
+        if not self.data['links']:
+            raise ValueError("No results. Try different initial url.")
+
+        self.df = {i: [data[i]] for i in self.display_content if i in self.content}
+        new = self.extract(count, return_type='pandas')
+        concatenated = pd.concat([data, new], columns=self.display_content)
+
+        return concatenated
 
 ###########################################################
 ####### queryScraper does not always produce output #######
@@ -146,3 +269,16 @@ class queryScraper:
         except:
             print("Error on scraping url: ", url)
         return data
+
+
+
+if __name__ == '__main__':
+    t = ThreadedExtractor(6, "https://pubmed.ncbi.nlm.nih.gov/38186573", content=['title', 'abstract', 'links'])
+    t.extract(50, 'csv')
+    #s = Scraper()
+    #d = s.extract("https://pubmed.ncbi.nlm.nih.gov/38186573")
+    #print(d.keys())
+    #for i in d['links']:
+     #   print(d['links'][i])
+    #for i in d:
+     #   print(d[i], end='\n\n')
